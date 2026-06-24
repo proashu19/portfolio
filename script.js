306a276d2584fc2ba0316d693edae71b079331df
Ashutosh Maurya <proashu19@gmail.com>
2026-06-24 12:54:44 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const text = [
+  "Full-Stack Developer",
+  "Node.js & MongoDB Enthusiast",
+  "B.Tech CSE Student"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === text.length) {
+    count = 0;
+  }
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing-text").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    setTimeout(() => {
+      index = 0;
+      count++;
+    }, 1500);
+  }
+
+  setTimeout(type, 120);
+})();
